5fb09fe131d8cd34b6aa83f31163d8df22ae637c
Lucoiso <lucas.oliveiravb@gmail.com>
2022-08-01 23:19:19 -0300

    Update ModularFeatures_ExtraActions.Build.cs

--- Source/ModularFeatures_ExtraActions.Build.cs
+++ Source/ModularFeatures_ExtraActions.Build.cs
@@ -24,7 +24,6 @@ public class ModularFeatures_ExtraActions : ModuleRules
 			"EnhancedInput",
 			"GameplayAbilities",
 			"GameplayTags",
-			"GameplayTasks",
 			"GameFeatures",
 			"ModularGameplay"
 		});
