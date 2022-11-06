0fdc2667fe44416d81c324155631fed9da6611bf
lucoiso <lucas.oliveiravb@gmail.com>
2022-11-06 13:27:07 -0300

    Fix typo

--- Source/Public/MFEA_Settings.h
+++ Source/Public/MFEA_Settings.h
@@ -36,11 +36,11 @@ public:
 	explicit UMFEA_Settings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
 	/* Work in Progress: If true, will auto bind the ability input directly using the given Input Action */
-	UPROPERTY(GlobalConfig, EditAnywhere, Category = "Settings")
+	UPROPERTY(GlobalConfig, EditAnywhere, Category = "Settings", Meta = (DisplayName = "Work in Progress: Enable Ability Auto Binding"))
 	bool bEnableAbilityAutoBinding;
 
 	/* Determine the binding mode that will be used by this plugin - This choice affects which function from IAbilityInputBinding interface will be used to bind the abilities */
-	UPROPERTY(GlobalConfig, EditAnywhere, Category = "Settings", Meta = (DisplayName = "Use InputID Enumeration Class", EditCondition = "!bEnableAbilityAutoBinding"))
+	UPROPERTY(GlobalConfig, EditAnywhere, Category = "Settings", Meta = (EditCondition = "!bEnableAbilityAutoBinding"))
 	EAbilityBindingMode AbilityBindingMode;
 
 	/* Enumeration class that will be used by the Ability System Component to manage abilities inputs if AbilityBindingMode is set to InputID */
